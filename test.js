1f04db538da7446cde4c1449c65e2d0cd8f2be2b
ishawakankar <iwakankar@hotmail.com>
2018-11-13 16:25:17 +0530

    chai assertions

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 // const fetch = require('node-fetch');
 chai.should();
+const expect = require('chai').expect;
 // var sinon = require('sinon');
 var express = require('express');
 const supertest = require('supertest');
@@ -9,8 +10,9 @@ const app = serverObject.app;
 // const server = serverObject.server;
 var chaiHttp = require('chai-http');
 chai.use(chaiHttp);
+const assert = chai.assert;
 
-describe('Route', function () {
+describe('Testing Express Routes:', function () {
   var getUserStub, request, route, serverRequest;
 
   beforeEach(function () {
@@ -18,61 +20,70 @@ describe('Route', function () {
     // app = express();
 
     request = supertest(app);
+    console.log('\n')
     // request = chai.request(app);
   });
 
-  it('should respond with res object', function (done) {
+  it('ExpressRoute:  /auth', function (done) {
     // getUserStub.returns(null);
 
     request.get('/auth')
+    //   .expect(302)
       .end(function (err, res) {
         if (err) throw err;
         // console.log(res)
         // res.should.not.console.error();
         
+        res.should.status(302)
+        // expect(2).to.equal(3)
         
-        console.log('\n')
-        console.log('ExpressRoute:  /auth')
-        console.log('Status: ' + res.status + ', ' + res.res.statusMessage)
-        console.log('Url: ' + res.headers.location)
-        console.log('ClientError: ' + res.clientError)
-        console.log('ServerError: ' + res.serverError)
+        // console.log('ExpressRoute:  /auth')
+        // console.log('Status: ' + res.status + ', ' + res.res.statusMessage)
+        // console.log('Url: ' + res.headers.location)
+        // console.log('ClientError: ' + res.clientError)
+        // console.log('ServerError: ' + res.serverError)
       });
 
     done();
   }),
 
-    it('should respond with res object', function (done) {
+    it('ExpressRoute:  /auth/gitlab', function (done) {
       // getUserStub.returns(null);
 
       request.get('/auth/gitlab')
         .end(function (err, res) {
           if (err) throw err;
           // console.log(res)
-          console.log('\n')
-          console.log('ExpressRoute:  /auth/gitlab')
-          console.log('Status: ' + res.status + ', ' + res.res.statusMessage)
-          console.log('Url: ' + res.headers.location)
-          console.log('ClientError: ' + res.clientError)
-          console.log('ServerError: ' + res.serverError)
+
+          res.should.status(302)
+
+          
+        //   console.log('ExpressRoute:  /auth/gitlab')
+        //   console.log('Status: ' + res.status + ', ' + res.res.statusMessage)
+        //   console.log('Url: ' + res.headers.location)
+        //   console.log('ClientError: ' + res.clientError)
+        //   console.log('ServerError: ' + res.serverError)
         });
 
       done();
     }),
 
-    it('should respond with res object', function (done) {
+    it('ExpressRoute:  /apps', function (done) {
       // getUserStub.returns(null);
 
       request.get('/apps')
         .end(function (err, res) {
           if (err) throw err;
           // console.log(res)
-          console.log('\n')
-          console.log('ExpressRoute:  /apps')
-          console.log('Status: ' + res.status + ', ' + res.res.statusMessage)
-          console.log('Url: ' + res.headers.location)
-          console.log('ClientError: ' + res.clientError)
-          console.log('ServerError: ' + res.serverError)
+          
+          res.should.status(200)
+
+          
+        //   console.log('ExpressRoute:  /apps')
+        //   console.log('Status: ' + res.status + ', ' + res.res.statusMessage)
+        //   console.log('Url: ' + res.headers.location)
+        //   console.log('ClientError: ' + res.clientError)
+        //   console.log('ServerError: ' + res.serverError)
         });
 
       done();
