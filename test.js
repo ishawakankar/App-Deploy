99ab2d5c96f568e8f3693f7e17e8769f2fb73b29
ishawakankar <iwakankar@hotmail.com>
2018-11-14 10:25:32 +0530

    updated test cases

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
 const chai = require('chai');
-// const fetch = require('node-fetch');
 chai.should();
-const expect = require('chai').expect;
-// var sinon = require('sinon');
 var express = require('express');
 const supertest = require('supertest');
 const serverObject = require('./server');
@@ -13,68 +10,61 @@ chai.use(chaiHttp);
 const assert = chai.assert;
 
 describe('Testing Express Routes:', function () {
-  var getUserStub, request, route, serverRequest;
+  var request;
 
   beforeEach(function () {
-    // getUserStub = sinon.stub();
-    // app = express();
 
     request = supertest(app);
-    console.log('\n')
+    console.log('\n');
     // request = chai.request(app);
   });
 
   it('ExpressRoute:  /auth', function (done) {
-    // getUserStub.returns(null);
 
     request.get('/auth')
     //   .expect(302)
       .end(function (err, res) {
         if (err) throw err;
-        // console.log(res)
-        // res.should.not.console.error();
         
         res.should.status(302)
         // expect(2).to.equal(3)
-        
-        // console.log('ExpressRoute:  /auth')
-        // console.log('Status: ' + res.status + ', ' + res.res.statusMessage)
-        // console.log('Url: ' + res.headers.location)
-        // console.log('ClientError: ' + res.clientError)
-        // console.log('ServerError: ' + res.serverError)
+        // chai.assert('')
       });
 
     done();
   }),
 
     it('ExpressRoute:  /auth/gitlab', function (done) {
-      // getUserStub.returns(null);
 
       request.get('/auth/gitlab')
         .end(function (err, res) {
           if (err) throw err;
-          // console.log(res)
 
           res.should.status(302)
-
-          
-        //   console.log('ExpressRoute:  /auth/gitlab')
-        //   console.log('Status: ' + res.status + ', ' + res.res.statusMessage)
-        //   console.log('Url: ' + res.headers.location)
-        //   console.log('ClientError: ' + res.clientError)
-        //   console.log('ServerError: ' + res.serverError)
         });
 
       done();
     }),
 
+    it('ExpressRoute:  /deploy', function (done) {
+
+        request.post('/deploy').send({
+          url: 'https://github.com/ishawakankar/test2'
+        })
+          .end(function (err, res) {
+            if (err) throw err;
+  
+            res.should.status(302)
+          });
+  
+        done();
+      }),
+
     it('ExpressRoute:  /apps', function (done) {
-      // getUserStub.returns(null);
 
       request.get('/apps')
         .end(function (err, res) {
           if (err) throw err;
-          // console.log(res)
           
           res.should.status(200)
 
