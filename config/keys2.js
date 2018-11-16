104a42face74b9a2b2dc857bbe112b06d1499357
Isha Wakankar <40451216+ishawakankar@users.noreply.github.com>
2018-11-16 09:44:38 +0530

    Delete keys2.js

--- config/keys2.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleKeys : {
-        clientID: "95c7db28c8fe0838e531170b7eb6e4dff3f78256ae651e5b6d7a24e1c29133e5",
-        clientSecret: "c6e3694d76c50ae1469e02d7bd98ede444066cf232f8cffa32f80a6ca85086f8"
-    }
-}
